c87a3e00830d6a6dd91c0c4f74020d227bdf390c
Connor Boyle <cjblink1@gmail.com>
2017-04-24 14:31:20 -0400

    Testing backend

---
+++ backend/express-test.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express()
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/api', function (req, res) {
+  res.send('hello world')
+})
+
+app.get('/api');
+
+app.listen(3000, function() {
+    console.log('listening on 3000');
+});
