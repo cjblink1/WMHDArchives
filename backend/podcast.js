bda5b05a511710a4e1d44db26f4a5edc4ddf5791
Luke Wukusick <lwukusick@gmail.com>
2017-04-27 13:37:56 -0400

    I guess i changed podcast.js

--- backend/podcast.js
+++ backend/podcast.js
@@ -5,7 +5,7 @@ var db = require('./database');
 router.get('/', function (req, res){
     console.log('About to execute query');
     db.execQuery('SELECT * FROM get_all_podcasts()', function(Qres){
-        console.log(JSON.stringify(Qres.rows[0]));
+        console.log('Executing all-podcasts query');
         res.send(JSON.stringify(Qres.rows));
     });
 });
